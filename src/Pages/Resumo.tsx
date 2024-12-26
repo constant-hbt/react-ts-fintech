250742891b6c2ef2c68530c094766585cb937c24
Henrique BT <henriqueborgesvotu@gmail.com>
2024-12-26 15:36:03 -0300

    :sparkles: feat: Listagem das vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -5,8 +5,49 @@ const Resumo = () => {
 
   const { data } = useData();
 
+  if (!data)
+    return null;
+
+  const valorTotal = data
+                      .reduce((acc, item) => acc + item.preco, 0)
+                      .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+  const valorRecebido = data
+                          .filter(item => item.status === 'pago')
+                          .reduce((acc, item) => acc + item.preco, 0)
+                          .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+  const valorProcessando = data
+                            .filter(item => item.status === 'processando')
+                            .reduce((acc, item) => acc + item.preco, 0)
+                            .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
   return (
-    <div>Resumo</div>
+    <section>
+      <div className='resumo flex mb'>
+        <div className='box'>
+          <div>
+            <h2>Vendas</h2>
+            <span>{valorTotal}</span>
+          </div>
+        </div>
+        <div className='box'>
+          <div>
+            <h2>Recebido</h2>
+            <span>{valorRecebido}</span>
+          </div>
+        </div>
+        <div className='box'>
+          <div>
+            <h2>Processando</h2>
+            <span>{valorProcessando}</span>
+          </div>
+        </div>
+      </div>
+      <div className='box mb'>
+        Gráficos
+      </div>
+    </section>
   )
 }
 
