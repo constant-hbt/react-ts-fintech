08ffd14dc999acd9365880fe490ce55fe25779be
Henrique BT <henriqueborgesvotu@gmail.com>
2024-12-26 16:00:57 -0300

    :sparkles: feat: Melhorando a sidenav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,14 +5,13 @@ import Meses from './Meses';
 
 const Header = () => {
 
-  const { data } = useData();
-
-  console.log(data);
+  const [title, setTitle] = React.useState('Resumo');
 
   return (
     <header className='mb'>
-      <div className='mb'>
+      <div className='mb daterange'>
         <DateRange/>
+        <h1 className='box bg-3'>{title}</h1>
       </div>
       <Meses/>
     </header>
